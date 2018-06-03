43a36b7e6543f84cbdadb2fd5508d2b18ff5ed6a
fatih.senturk <fatih.senturk@n11.com>
2018-06-03 18:35:13 +0300

    constructed point class

--- src/main/java/com/fath/billiard/Point.java
+++ src/main/java/com/fath/billiard/Point.java
@@ -1,4 +1,21 @@
 package com.fath.billiard;
 
 public class Point {
+
+    int x;
+    int y;
+    int vX;
+    int vY;
+
+    Point(int x, int y, int vX, int vY) {
+        this.x = x;
+        this.y = y;
+        this.vX = vX;
+        this.vY = vY;
+    }
+
+    static Point make(int x, int y, int vX, int vY) {
+        return new Point(x, y, vX, vY);
+    }
+    
 }
