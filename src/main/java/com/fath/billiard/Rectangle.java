a0b0b2e495cf88e2d238d72fbe0c8d619a4e4910
fatih.senturk <fatih.senturk@n11.com>
2018-06-03 18:40:03 +0300

    constructed Rectangle

--- src/main/java/com/fath/billiard/Rectangle.java
+++ src/main/java/com/fath/billiard/Rectangle.java
@@ -1,4 +1,19 @@
 package com.fath.billiard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rectangle {
+
+    private int N;
+    private int M;
+    private Point point;
+
+    private List<Point> crashedList = new ArrayList<>(); // stored
+
+    public Rectangle(int N, int M, Point point) {
+        this.N = N;
+        this.M = M;
+        this.point = point;
+    }
 }
