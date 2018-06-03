e9fea56e5c4eaaac23cda2ff8b47cc3955f72fd3
fatih.senturk <fatih.senturk@n11.com>
2018-06-03 18:51:56 +0300

    Implement finding next point

--- src/main/java/com/fath/billiard/Rectangle.java
+++ src/main/java/com/fath/billiard/Rectangle.java
@@ -18,6 +18,48 @@ public class Rectangle {
     }
 
     public void solveProblem() {
+        Point nextPoint = findNextPoint(this.point);
+        while (true) {
+            boolean contains = pointIsASpot(nextPoint);
+            if (!contains) {
+                nextPoint = findNextPoint(nextPoint);
+            } else {
+                System.out.println(nextPoint.x + "," + nextPoint.y);
+                break;
+            }
+        }
+    }
 
+    private Point findNextPoint(Point point) {
+        // find next points x and y axis
+        int returnX = point.x;
+        int returnY = point.y;
+        while (true) {
+            returnX = returnX + point.vX;
+            returnY = returnY + point.vY;
+            if (returnX == N || returnX == 0 || returnY == M || returnY == 0) {
+                break;
+            }
+        }
+
+        // find next points vX and vY
+        int returnVX;
+        int returnVY;
+        if (returnX == N || returnX == 0) {
+            returnVX = point.vX * -1;
+            returnVY = point.vY;
+        } else {
+            returnVX = point.vX;
+            returnVY = point.vY * -1;
+        }
+
+        Point nextPoint = Point.make(returnX, returnY, returnVX, returnVY);
+        System.out.println("Crashed: " + nextPoint);
+
+        return nextPoint;
+    }
+
+    private boolean pointIsASpot(Point nextPoint) {
+        return false;
     }
 }
