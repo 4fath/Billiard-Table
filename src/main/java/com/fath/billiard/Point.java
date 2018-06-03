7fcad933d2aeb8826b49137c4600f46f510cf90e
fatih.senturk <fatih.senturk@n11.com>
2018-06-03 18:36:52 +0300

    Implement equals and toString method - Point.class

--- src/main/java/com/fath/billiard/Point.java
+++ src/main/java/com/fath/billiard/Point.java
@@ -17,5 +17,26 @@ public class Point {
     static Point make(int x, int y, int vX, int vY) {
         return new Point(x, y, vX, vY);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point) {
+            Point otherPoint = (Point) obj;
+            if (otherPoint.x == this.x && otherPoint.y == this.y && otherPoint.vX == this.vX && otherPoint.vY == this.vY) {
+                return true;
+            }
+        }
+        return false;
+    }
     
+    @Override
+    public String toString() {
+        return "Point:{ " +
+                "x= " + x +
+                ", y= " + y +
+                ", vX= " + vX +
+                ", vY= " + vY +
+                " }";
+    }
+
 }
